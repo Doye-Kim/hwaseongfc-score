db5c1d0b630c1abf08ba1433a9315109adf314fc
김도예 <kmdy125@gmail.com>
2026-02-21 18:43:18 +0900

    feat: 로딩 스피터 구현

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import styles from './LoadingSpinner.module.css';
+import hwaseongLogo from '@/assets/logos/hwaseong.png';
+
+const LoadingSpinner = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logoWrap}>
+        <img src={hwaseongLogo} alt='화성FC' className={styles.logo} />
+        <div className={styles.spinner} />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
